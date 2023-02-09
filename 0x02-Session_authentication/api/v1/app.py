7b91afbd69e57657da6d59a59c9aa82c4e234595
VikitorChidi <victorblessed9@gmail.com>
2023-02-09 08:05:41 +0100

    new task

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,14 +10,11 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 auth = None
 auth_type = getenv("AUTH_TYPE", 'auth')
-
-# if auth_type == "basic_auth":
-#     auth = BasicAuth()
-# elif auth_type == "auth":
-#     auth = Auth()
 
 
 app = Flask(__name__)
@@ -30,6 +27,12 @@ elif auth_type == "basic_auth":
     auth = BasicAuth()
 elif auth_type == "auth":
     auth = Auth()
+elif auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    auth = SessionDBAuth()
+elif auth_type == "no_auth":
+    auth = None
 
 
 @app.errorhandler(404)
